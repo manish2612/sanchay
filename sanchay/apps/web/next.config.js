3d6668f6dd4e328f26d1880279b4b5c99769a97b
Manish Mahajan <manish.2612@hotmail.com>
2025-12-23 00:14:50 +0530

    IMP: build creation change

--- sanchay/apps/web/next.config.js
+++ sanchay/apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ["@sanchay/ui", "@sanchay/config", "@sanchay/utils"],
+    output: "standalone",
+    transpilePackages: ["@sanchay/ui", "@sanchay/config", "@sanchay/utils", "@sanchay/theme-provider", "@sanchay/theme-adapters", "@sanchay/design-tokens"],
 };
 
 module.exports = nextConfig;
