3c73dbbbcae54567cfde2b1b9052bfd6357fa5ff
Manish Mahajan <manish.2612@hotmail.com>
2025-12-25 16:55:24 +0530

    Web: commong Text component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { Button } from './primitives/Button';
 export { TextInput } from './primitives/TextInput';
+export { Text } from './primitives/Text/Text.dom';
 export { GridBackground } from './components/GridBackground';
