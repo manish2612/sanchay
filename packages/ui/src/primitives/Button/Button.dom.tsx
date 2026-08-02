4f1fbf11b05707d4c9cb23892a08afdb0fb8ea44
Manish Mahajan <manish.2612@hotmail.com>
2026-08-02 18:49:59 +0530

    Button primitive text size change

--- packages/ui/src/primitives/Button/Button.dom.tsx
+++ packages/ui/src/primitives/Button/Button.dom.tsx
@@ -19,9 +19,9 @@ const buttonVariants = cva(
           "bg-danger text-danger-foreground hover:bg-danger/90 focus-visible:ring-offset-2 focus-visible:ring-danger",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-10 px-4 py-2 text-sm",
+        sm: "h-9 rounded-md px-3 text-xs",
+        lg: "h-11 rounded-md px-8 text-base",
         icon: "h-10 w-10",
       },
     },
