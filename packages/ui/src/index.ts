da49e99f5890c6f67478bacf55842b948683435f
Manish Mahajan <manish.2612@hotmail.com>
2026-08-11 12:58:07 +0530

    feat: implement Tooltip primitive and add to UI library dependencies

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -24,3 +24,5 @@ export * from './components/SplitButton';
 export * from './primitives/SegmentedControl';
 export * from './primitives/Sheet';
 export * from './components/FormWizard';
+export * from './primitives/Tooltip';
+
